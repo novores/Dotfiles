d387363894ff0c5c5201919b8c87be632be44436
Noval Hidayat <novalhidayat@tuta.io>
2022-06-28 23:19:19 +0700

    terhapus:        bin/dwmstatus/bar.sh 	terhapus:        bin/dwmstatus/gruvbox 	terhapus:        bin/run.sh 	terhapus:        bin/xautolock.sh 	terhapus:        dwm/LICENSE 	terhapus:        dwm/Makefile 	terhapus:        dwm/README 	terhapus:        dwm/config.def.h 	terhapus:        dwm/config.h 	terhapus:        dwm/config.mk 	terhapus:        dwm/drw.c 	terhapus:        dwm/drw.h 	terhapus:        dwm/drw.o 	terhapus:        dwm/dwm 	terhapus:        dwm/dwm.1 	terhapus:        dwm/dwm.c 	terhapus:        dwm/dwm.o 	terhapus:        dwm/dwm.png 	terhapus:        dwm/patch/dwm-alwayscenter-20200625-f04cac6.diff 	terhapus:        dwm/patch/dwm-cool-autostart-6.2.diff 	terhapus:        dwm/patch/dwm-hide_vacant_tags-6.3.diff 	terhapus:        dwm/patch/dwm-status2d-systray-6.3.diff 	terhapus:        dwm/transient.c 	terhapus:        dwm/util.c 	terhapus:        dwm/util.h 	terhapus:        dwm/util.o 	terubah:         xinitrc

--- bin/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-while type dwm >/dev/null; do 
-  dwm && continue || break 
-done
--- bin/xautolock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-xautolock \
-  -time 7 -locker "betterlockscreen -l dim" -killtime 30 \
-  -killer "loginctl suspend" -notify 30 \
-  -notifier "notify-send 'NOTICE' 'sebentar lagi dikunci'" & \
