52fe8a688b3b6db6d9514783bf547b5da3a8388e
Novores <novalhidayat@tuta.io>
2023-01-10 02:12:09 +0700

    update

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,9 +4,9 @@ config.load_autoconfig()
 config.source('gruvbox.py')
 
 # key binding
-config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc --key 4C40A8C5', mode='insert')
-config.bind('pw', 'spawn --userscript qute-keepassxc --key 4C40A8C5', mode='normal')
-config.bind('pt', 'spawn --userscript qute-keepassxc --key 4C40A8C5 --totp', mode='normal')
+# config.bind('<Alt-Shift-u>', 'spawn --userscript qute-keepassxc --key 4C40A8C5', mode='insert')
+# config.bind('pw', 'spawn --userscript qute-keepassxc --key 4C40A8C5', mode='normal')
+# config.bind('pt', 'spawn --userscript qute-keepassxc --key 4C40A8C5 --totp', mode='normal')
 
 config.bind(';t', 'hint userscript link translate')
 config.bind(';T', 'hint userscript all translate --text')
@@ -15,7 +15,7 @@ config.bind('<Ctrl+Shift+T>', 'spawn --userscript translate --text')
 config.bind('K', 'tab-next')
 config.bind('J', 'tab-prev')
 
-config.bind(',D', 'config-cycle colors.webpage.darkmode.enabled')
+config.bind(',D', 'config-cycle colors.webpage.darkmode.enabled ;; restart')
 config.bind(',ap', 'config-cycle content.user_stylesheets $HOME/Gitrepos/solarized-everything-css/css/apprentice/apprentice-all-sites.css ""')
 config.bind(',dr', 'config-cycle content.user_stylesheets $HOME/Gitrepos/solarized-everything-css/css/darculized/darculized-all-sites.css ""')
 config.bind(',gr', 'config-cycle content.user_stylesheets $HOME/Gitrepos/solarized-everything-css/css/gruvbox/gruvbox-all-sites.css ""')
